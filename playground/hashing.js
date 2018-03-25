334827b7a63a33ec427d79677659182ef3a8aead
Khizer Syed <khizer44@gmail.com>
2018-03-25 15:28:48 +0530

    Adding hashed passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 var {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id : 5
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log(`decoded :`,JSON.stringify(decoded, undefined, 2));
+var hashedPassword = '$2a$10$fDuCthGEEYQ3PTeaiNR8UOmsokyIEhKkHYeSbsoBrYRNQh8WF1Y86';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id : 5
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(`decoded :`,JSON.stringify(decoded, undefined, 2));
 // var message = 'I am Khizer';
 //
 // var hash = SHA256(message).toString();
